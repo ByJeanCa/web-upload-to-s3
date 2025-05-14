a3fbc941f43812ce52ce58a923e0d71185138871
Jean Carlo Alvarado <105259214+ByJeanCa@users.noreply.github.com>
2025-05-14 12:09:19 -0600

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-echo "Checking if python is on the system"
-if ! command -v python3 > dev/null/2>&1; then 
-    echo "Installing Python"
-    yum install python3 -y
-echo "Python installed"
+echo "Checking if Python is on the system..."
+if ! command -v python3 > /dev/null 2>&1; then 
+    echo "Installing Python..."
+    sudo yum install python3 -y
+    echo "Python installed"
+else
+    echo "Python already installed"
+fi
 
-echo "Checking if pip is on the system"
-if ! command -v pip > dev/null/2&1; then
-    echo "Installing Python"
-    yum install pip
-echo "Pip installed"
+echo "Checking if pip is on the system..."
+if ! command -v pip3 > /dev/null 2>&1; then
+    echo "Installing pip..."
+    sudo yum install python3-pip -y
+    echo "Pip installed"
+else
+    echo "Pip already installed"
+fi
 
 echo "Installing dependencies..."
 
-pip install flask boto3
-pip install python-dotenv 
+pip3 install flask boto3 python-dotenv
 
-echo "Dependencies sucessfully installed"
-
+echo "Dependencies successfully installed"
